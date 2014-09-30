28fe6aefaa7874431c477cc9a7853f6f87c026b6
ARACOOOL <tkdforever@gmail.com>
2014-10-01 00:26:25 +0500

    - code review

---
+++ dl.php
@@ -0,0 +1,7 @@
+<?php
+$file = urldecode($_GET['link']);
+$filename = $_GET['name'];
+header("Content-type: octet/stream");
+header("Content-disposition: attachment; filename=" . $filename . ";");
+readfile($file);
+exit;
